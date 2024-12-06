3d09265d3376c7e3372d8dff1cf5120c4d4a6831
Maxim Botkin <68293738+MaximBotkin@users.noreply.github.com>
2024-12-06 15:18:30 +0300

    solve pep8

--- Лабораторная работа 1/Задание 2/task2.py
+++ Лабораторная работа 1/Задание 2/task2.py
@@ -3,7 +3,7 @@ from task1 import Train, Student, Car
 if __name__ == "__main__":
     train = Train(500, 350)
     student = Student(2, 4.5)
-    car = Car(55, 40,12)
+    car = Car(55, 40, 12)
 
     try:
         train.set_new_train_params('500', '500')
